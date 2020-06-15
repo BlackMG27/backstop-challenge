27e8b594b2d3beb0f4cec9968a7215559018c985
ike <ike@iMac.attlocal.net>
2020-06-14 22:52:38 -0400

    got the create-react-app setup and started on store.js

---
+++ pokemon-redux/src/App.js
@@ -0,0 +1,22 @@
+import React, {Fragment} from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <Fragment>
+      <header>
+          <nav className="nav">
+            <ul>
+              <li>Top</li>
+            </ul>
+          </nav>
+          <h1 className="title">Pokemon Redux</h1>
+      </header>
+      <section className="pokemon-cards">
+
+      </section>
+    </Fragment>
+  );
+}
+
+export default App;
